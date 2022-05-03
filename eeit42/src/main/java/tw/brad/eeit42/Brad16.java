7765c0a85a82a307df6cd5225a27931540dd2050
USER <brad@brad.tw>
2022-05-03 14:28:49 +0800

    05031428

--- eeit42/src/main/java/tw/brad/eeit42/Brad16.java
+++ eeit42/src/main/java/tw/brad/eeit42/Brad16.java
@@ -16,14 +16,20 @@ public class Brad16 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String x = request.getParameter("x");
 		String y = request.getParameter("y");
+		String op = request.getParameter("op");
 		String r = request.getParameter("result");
 		
+		if (op == null) op = "";
+		String op1 = op.equals("1")?"selected":"";
+		String op2 = op.equals("2")?"selected":"";
+		String op3 = op.equals("3")?"selected":"";
+		String op4 = op.equals("4")?"selected":"";
 		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
 		String html = BradUtils.loadView();
-		out.print(String.format(html, x, y, r));
+		out.print(String.format(html, x, op1, op2, op3, op4, y, r));
 		
 		response.flushBuffer();
 	}
