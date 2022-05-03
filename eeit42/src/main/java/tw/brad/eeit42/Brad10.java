2eb839880b65de4d8feec5971926f49878745376
USER <brad@brad.tw>
2022-05-03 11:14:56 +0800

    05031114

--- eeit42/src/main/java/tw/brad/eeit42/Brad10.java
+++ eeit42/src/main/java/tw/brad/eeit42/Brad10.java
@@ -3,6 +3,7 @@ package tw.brad.eeit42;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,12 +16,14 @@ public class Brad10 extends HttpServlet {
 			throws ServletException, IOException {
 		
 		request.setCharacterEncoding("UTF-8");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("brad11.html");
 		
 		//-------------------
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.print("<h1>Brad Big Company</h1>");
 		out.print("<hr />");
+		dispatcher.include(request, response);
 		out.print("<hr />");
 		out.print("Copyleft");
 		response.flushBuffer();
