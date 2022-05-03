b744aa7af66dd33a352caf84c790b0f1b7c61c81
USER <brad@brad.tw>
2022-05-03 13:20:51 +0800

    05031320

---
+++ eeit42/src/main/java/tw/brad/eeit42/Brad14.java
@@ -0,0 +1,27 @@
+package tw.brad.eeit42;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad14")
+public class Brad14 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		// Controller
+		// 1. 接收參數, 整理
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		
+		// 2. Model => 演算法
+		Brad15 model = new Brad15(x, y);
+		double result = model.operator();
+		System.out.println(result);
+		
+		// 3. 呈現 => View
+	}
+
+}
---
+++ eeit42/src/main/java/tw/brad/eeit42/Brad15.java
@@ -0,0 +1,17 @@
+package tw.brad.eeit42;
+
+public class Brad15 {
+	private int x, y;
+	public Brad15(String x, String y) {
+		try {
+			this.x = Integer.parseInt(x);
+			this.y = Integer.parseInt(y);
+		}catch (Exception e) {
+		}
+	}
+	public double operator() {
+		double r = 0;
+		r = x + y;
+		return r;
+	}
+}
