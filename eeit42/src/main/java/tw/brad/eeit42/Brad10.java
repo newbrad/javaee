62e2a3b80efecd31abb7a79f095a0e96400ff409
USER <brad@brad.tw>
2022-05-03 11:38:22 +0800

    05031138

--- eeit42/src/main/java/tw/brad/eeit42/Brad10.java
+++ eeit42/src/main/java/tw/brad/eeit42/Brad10.java
@@ -18,16 +18,22 @@ public class Brad10 extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 //		RequestDispatcher dispatcher = 
 //				request.getRequestDispatcher("brad11.html");
+		
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		
 		RequestDispatcher dispatcher = 
-				request.getRequestDispatcher("Brad11");
+				request.getRequestDispatcher("Brad11?z=100");
 		
 		//-------------------
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.print("<h1>Brad Big Company</h1>");
+		out.print(x + " : " + y );
 		out.print("<hr />");
 		dispatcher.include(request, response);
 		out.print("<hr />");
+		
 		out.print("Copyleft");
 		response.flushBuffer();
 		
--- eeit42/src/main/java/tw/brad/eeit42/Brad11.java
+++ eeit42/src/main/java/tw/brad/eeit42/Brad11.java
@@ -12,8 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Brad11")
 public class Brad11 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		String z = request.getParameter("z");
+		
 		PrintWriter out = response.getWriter();
-		out.print("Hello, Brad11");
+		out.print("Hello, Brad11<br>");
+		out.print("Brad11: " + x + ":" + y + ":" + z);
 		response.flushBuffer();
 	
 	}
