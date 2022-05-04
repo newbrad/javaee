19d9327ae18782a852ef29c1f61c09278176ef47
USER <brad@brad.tw>
2022-05-04 13:28:44 +0800

    05041328

--- eeit42/src/main/java/tw/brad/eeit42/Brad24.java
+++ eeit42/src/main/java/tw/brad/eeit42/Brad24.java
@@ -1,15 +1,34 @@
 package tw.brad.eeit42;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tw.brad.utils.BCrypt;
+
 @WebServlet("/Brad24")
 public class Brad24 extends HttpServlet {
+	private Connection conn;
+	
     public Brad24() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/iii", prop);
+		}catch (Exception e) {
+			System.out.println(e.toString());
+		}
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -39,7 +58,20 @@ public class Brad24 extends HttpServlet {
 		throws Exception
 		{
 	
-		return false;
+		boolean isRight = false;
+		String sql = "SELECT * FROM member WHERE account = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, account);
+		ResultSet rs = pstmt.executeQuery();
+		if (rs.next()) {
+			String hashPasswd = rs.getString("passwd");
+			if (BCrypt.checkpw(passwd, hashPasswd)) {
+				isRight = true;
+			}
+		}
+		
+		
+		return isRight;
 	}
 	
 }
