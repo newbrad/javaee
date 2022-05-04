fb5f79d139bcf34f7bff580746f1fb13df1d3311
USER <brad@brad.tw>
2022-05-04 15:31:31 +0800

    05041531

---
+++ eeit42/src/main/java/tw/brad/eeit42/Brad25.java
@@ -0,0 +1,22 @@
+package tw.brad.eeit42;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad25")
+public class Brad25 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ eeit42/src/main/webapp/brad35.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Brad Big Company</title>
+</head>
+<body>
+
+<form action="brad36.jsp" method="get">
+	<input type="text" name="account" /><br />
+	<input type="submit" value="OK">
+</form>
+
+</body>
+</html>
